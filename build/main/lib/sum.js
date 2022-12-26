0fdcf6274a080fe3f84d028dd0159430ce383620
Jackson Chadfield <contact@jacksonc.dev>
2022-12-26 20:21:45 +1300

    add formatting and linting tools

---
+++ build/main/lib/sum.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = void 0;
+function sum(a, b) {
+  return a + b;
+}
+exports.sum = sum;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3VtLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL2xpYi9zdW0udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsU0FBZ0IsR0FBRyxDQUFDLENBQVMsRUFBRSxDQUFTO0lBQ3RDLE9BQU8sQ0FBQyxHQUFHLENBQUMsQ0FBQztBQUNmLENBQUM7QUFGRCxrQkFFQyJ9
